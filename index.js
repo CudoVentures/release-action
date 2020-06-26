948c951833acd8288fbc84880fd006f39b4d7a2e
Dexter Edwards <dexter.edwards@cudoventures.com>
2020-06-26 14:26:05 +0100

    remove unused code

--- index.js
+++ index.js
@@ -15,13 +15,12 @@ const run = async () => {
     });
     const json = response.json()
     console.log(json)
-    // `who-to-greet` input defined in action metadata file
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+
+    // const time = (new Date()).toTimeString();
+    // core.setOutput("time", time);
+    // // Get the JSON webhook payload for the event that triggered the workflow
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`);
   } catch (error) {
     core.setFailed(error.message);
   }
