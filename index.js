82628b369199eb29317216c29b4a984d8c04e471
Dexter Edwards <dexter.edwards@cudoventures.com>
2020-06-26 14:21:14 +0100

    get prs from github search api

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+const run = async () => {
+  try {
+    const repo = core.getInput('repo');
+    const githubToken = process.env.GITHUB_TOKEN
+    const dateToday = new Date().toISOString().split('T')[0]
+    const prEndpoint = `https://api.github.com/search/issues?q=repo:${repo}+is:pr+is:merged+sort:merged-date+merged:${dateToday}`
+    const response = await fetch(prEndpoint, {
+      headers: {
+        Authorization: `token ${githubToken}`
+      }
+    });
+    const json = response.json()
+    console.log(json)
+    // `who-to-greet` input defined in action metadata file
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+} 
+
+run()
